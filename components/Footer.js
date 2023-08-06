9a8602fb9457a6a0699bdd9e2938f407ae1b8ade
ines <insanlur@gmail.com>
2023-08-06 15:54:37 +0200

    Revert "Update modal style"
    This reverts commit 408d223b1c31a94e0797dc6e9467decc13e5d6d9.

--- components/Footer.js
+++ components/Footer.js
@@ -1,27 +1,33 @@
 import React from 'react'
 import style from './Footer.module.css'
+import Link from 'next/link'
 import { useRouter } from 'next/router'
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
 
 function Footer({
-    onInfoClick
 }) {
     const router = useRouter();
+    const currentPath = router.pathname;
+
     return (
-        <>
-            <div className={style.footer}>
-                <button onClick={onInfoClick}>Cómo se calcula?</button>
-                <p>
-                    <span>Realizado por</span>
-                    <a 
-                        href="https://www.linkedin.com/in/ines-sanluis/"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Inés San Luís
-                    </a>
-                </p>
-            </div>
-        </>
+        <div className={style.footer}>
+            {currentPath === "/" ? <Link href="/about">
+                Cómo se calcula?
+            </Link> : <Link href="/">
+                <ArrowBackIosIcon></ArrowBackIosIcon>
+                Atrás
+            </Link>}
+            <p>
+                <span>Realizado por</span>
+                <a 
+                    href="https://www.linkedin.com/in/ines-sanluis/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Inés San Luís
+                </a>
+            </p>
+        </div>
     )
 }
 
--- pages/index.js
+++ pages/index.js
@@ -10,8 +10,6 @@ import Range from "../components/Range";
 import Time from "../components/Time";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-import { Modal } from "@mui/material";
-import {AiFillCloseCircle} from "react-icons/ai";
 // 7:30 in milliseconds
 const SHIFT_DURATION = 7 * 60 * 60 * 1000 + 30 * 60 * 1000;
 
@@ -48,21 +46,6 @@ export default function Home() {
   const [compensateSecondRange, setCompensateSecondRange] = useState("00:00");
   const [compensateThirdRange, setCompensateThirdRange] = useState("00:00");
   const [showExtraTime, setShowExtraTime] = useState(false);
-  const [open, setOpen] = useState(false);
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
-  const modalStyle = {
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    backgroundColor: 'white',
-    boxShadow: 24,
-    padding: '2rem',
-    borderRadius: '0.5rem',
-    overflow: 'scroll',
-    height: '80%'
-  };
 
   useEffect(() => {
     // change document title
@@ -254,69 +237,7 @@ export default function Home() {
           </div>
         </div>
       </main>
-      <Footer
-        onInfoClick={handleOpen}
-      />
-      <Modal
-          style={modalStyle}
-          open={open}
-          onClose={handleClose}
-          hideBackdrop={true}
-          aria-labelledby="modal-title" aria-describedby="modal-description"
-      >
-          <>
-              <div style={{ 
-                display: 'flex', 
-                justifyContent: 'flex-end'
-              }}>
-                <button onClick={handleClose}>
-                  <AiFillCloseCircle size={30} color={"#009bbb"} />
-                </button>
-              </div>
-              <h1 id="modal-title">Cómo se calcula?</h1>
-              <section>
-                  <h2 id="modal-description">
-                      Artigo 61. Complemento de dispoñibilidade
-                  </h2>
-                  <ol>
-                      <li>
-                      Aplicarase ao persoal que, previa designación pola empresa, polas características do seu posto de traballo,
-                      voluntariamente estea disposto a modificacións constantes da súa xornada e do seu horario de luns a domingo.
-                      </li>
-                      <ol>
-                      <li>
-                          Cómputo semanal. Percibirao o/a traballador/a que estea suxeito ás condicións do artigo 34 durante unha (1) semana.
-                      </li>
-                      <li>
-                          Cómputo mensual. Percibirao o/a traballador/a que estea suxeito ás condiciones do artigo 34 durante un (1) mes completo.
-                      </li>
-                      </ol>
-                  </ol>
-              </section>
-              <section>
-                  <h2>
-                      Artigo 34. Xornada superior á ordinaria
-                  </h2>
-                  <ol start="6">
-                      <li>
-                      As convocatorias respectarán o descanso entre xornadas de 12 horas e, con carácter xeral, deben respectar o límite de 9 horas por día.
-          Cando por necesidades da produción se supere dito límite, sen superar nunca as catorce (14) horas por xornada, as horas de exceso computaranse do seguinte xeito: 
-                      </li>
-                      <ol>
-                      <li>
-                      Cando se realicen xornadas que superando as 9 horas non superen as 12 horas, computaranse a razón de 1,5 horas por hora de exceso. 
-                      </li>
-                      <li>
-                      Cando pasen as 12 horas sen pasar as 14, computaranse a razón de 2 horas por hora de exceso. 
-                      </li>
-                      <li>
-                      Non se computará como traballo o tempo dedicado a comida ou cea, respectando o establecido no artigo 33.5.1 deste convenio.
-                      </li>
-                      </ol>
-                  </ol>
-              </section>
-          </>
-      </Modal>
+      <Footer></Footer>
     </>
   );
 }
